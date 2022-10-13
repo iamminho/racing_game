b442bb2d7107a3ec96e0214a0c55b9156ef42614
iamminho <minho100227@kakao.com>
2022-10-13 12:39:39 +0900

    feat(scores): car들이 간 거리를 저장하는 scores 클래스 생성

---
+++ src/main/java/model/Scores.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.HashMap;
+
+public class Scores {
+	public static HashMap<String, Integer> scores = new HashMap<>();
+
+	public HashMap<String, Integer> getScores() {
+		return scores;
+	}
+}
